d1eb062edaa1c739a533d56ede68bde21a1741de
Labvanced <44515367+Labvanced@users.noreply.github.com>
2023-02-01 11:33:58 +0700

    Update labvanced_serial_demo.py

--- labvanced_serial_demo.py
+++ labvanced_serial_demo.py
@@ -1,41 +1,102 @@
 #!/usr/bin/python   
-# Labvanced External Device Synchronization Example.
-
-# This server waits to receive a "set_rts_on" message or a "set_rts_off" message from a running experiment.
-# It sets the rts serial line to on or off depending on the message.
+# Labvanced Send triggers via serial port example.
 
 # The following packages must be installed on your system
+# you can use the pip package manager to install them
 import asyncio
 import json
 import websockets
 import serial
+import time
+import sys
+import glob
+PulseWidth = 0.01
+serial_ports = []
+scan_serial_ports = True
+SERIAL_PORT = "COM3" #e.g COM3
+IP_ADDRESS = 'localhost'
+WEBSOCKET_PORT = 8081
 
-# You need to change the port name depending on your hardware setup:
-# Usually on windows the port name is something like "COM3" and on linux something like "/dev/ttyS3"
-ser = serial.Serial(port="COM3") 
-ser.rts = False
 
 async def on_connect(websocket, path):
+    print("websocket connection established")
+    # set up serial port connection
+    try:
+        ser_port = serial.Serial(port=SERIAL_PORT) 
+        ser_port.write([0x00])
+        time.sleep(PulseWidth)
+    except:
+        print("Error connecting to serial port: {}".format(SERIAL_PORT))
+        raise Exception("Error connecting to serial port")
+
+    print("Successful connected to serial port: {}".format(SERIAL_PORT) )
+
     try:
         async for message in websocket:
             data = json.loads(message)
             # data has two fields 'msg' and 'data'. msg is the trigger / message, value is the variable value, both
             # defined in the 'send external trigger action' in your Labvanced experiment.
-            received_value = data['value']
+            received_value = data['value'] # you might not need value if you just want to send a trigger type
             received_msg = data['msg']
-
             # here you can make an if-elif for each of your triggers and depending on the type execute different code
-            if received_msg == 'set_rts_on':
-                print("setting rts line to on")
-                ser.rts = True # this sets the RTS line voltage
-            elif received_msg == 'set_rts_off':
-                print("setting rts line to off")
-                ser.rts = False # this sets the RTS line voltage
+            if received_msg == 'trigger1':
+                # the code inside here must be changed according to the vendor/trigger specification you want to send.
+                print("trigger 1 was received")
+                ser_port.write([0x01])
+                # waiting and writing again for brain product EEG 
+                time.sleep(PulseWidth)
+                ser_port.write([0x00])
+
+            elif received_msg == 'trigger2':
+                # the code inside here must be changed according to the vendor/trigger specification you want to send.
+                print("trigger 2 was received")
+                ser_port.write([0x02])
+                # waiting and writing again for brain product EEG 
+                time.sleep(PulseWidth)
+                ser_port.write([0x00])
             else:
-                print("unsupported event: {}".format(data))
+                # unsupported trigger
+                print("unsupported trigger: {}".format(received_msg))
     finally:
         print("connection lost")
 
-# Make sure that the IP address and port match with the Labvanced study settings.
-asyncio.get_event_loop().run_until_complete(websockets.serve(on_connect, 'localhost', 8081))
-asyncio.get_event_loop().run_forever()
+
+def get_serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+if(scan_serial_ports):
+    serial_ports = get_serial_ports()
+    if len(serial_ports)>0:
+        SERIAL_PORT = serial_ports[0]
+
+if SERIAL_PORT:
+    asyncio.get_event_loop().run_until_complete(websockets.serve(on_connect, IP_ADDRESS, WEBSOCKET_PORT))
+    asyncio.get_event_loop().run_forever()
+else:
+    print("no serial port found or specified")
+
